8494849a153f47b517f3fdd6e1ab947c1ab82340
Kavya Gupta <rkgqwe@gmail.com>
2023-07-18 12:11:59 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -11,10 +11,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
-app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	next();
-});
+// app.use((req, res, next) => {
+// 	res.header("Access-Control-Allow-Origin", "*");
+// 	next();
+// });
 
 
 const userConnection = mongoose.createConnection(db_token, {
