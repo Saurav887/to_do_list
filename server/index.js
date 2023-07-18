a75f1d11b40f6636f72ba7674f7045efa965350e
Kavya Gupta <rkgqwe@gmail.com>
2023-07-18 12:03:50 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -183,3 +183,20 @@ app.listen(port, () => {
 });
 
 export { app };
+
+module.exports = (req, res) => {
+    //set header first to allow request or origin domain (value can be different)
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
+
+//---- other code
+
+ //Preflight CORS handler
+    if(req.method === 'OPTIONS') {
+        return res.status(200).json(({
+            body: "OK"
+        }))
+    }
+}
